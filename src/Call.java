9ebb47db8ca258a75105020f00cf83653b9381be
Eric Sans <esansvi@gmail.com>
2022-11-19 21:58:51 +0100

    Modified few lines

--- src/Call.java
+++ src/Call.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class Call {
+public class Call {
+
+    private int sourceNumber=0;
+    private int destinationNumber=0;
+    private int duration=0;
+    private int band=0;
+    private boolean local=true;
+
+    public Call (int sourceNumber, int destinationNumber, int duration, int band, boolean local){
+        this.sourceNumber=sourceNumber;
+        this.destinationNumber=destinationNumber;
+        this.duration=duration;
+        this.band=band;
+        this.local=local;
+    }
+    public int getSourceNumber() {
+        return sourceNumber;
+    }
+    public void setSourceNumber(int sourceNumber) {
+        this.sourceNumber = sourceNumber;
+    }
+    public int getDestinationNumber() {
+        return destinationNumber;
+    }
+    public void setDestinationNumber(int destinationNumber) {
+        this.destinationNumber = destinationNumber;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public int getBand() {
+        return band;
+    }
+    public void setBand(int band) {
+        this.band = band;
+    }
+
+    public boolean isLocal() {
+        return local;
+    }
+    public void setLocal(boolean local) {
+        this.local = local;
+    }
+
+    @Override
+    public String toString() {
+        return "Call{" +
+                "sourceNumber=" + sourceNumber +
+                ", destinationNumber=" + destinationNumber +
+                ", duration=" + duration +
+                ", band=" + band +
+                ", is local:" + local +
+                '}';
+    }
+
+    public double callCost() {
+        double totalCost=0;
+        if (this.local == true){
+            totalCost = (15* this.duration);
+        } else if (this.band==1) {
+            totalCost = (20* this.duration);
+        } else if (this.band==2) {
+            totalCost = (25* this.duration);
+        } else {
+            totalCost = (30* this.duration);
+        }
+        return totalCost;
+    }
+
 }
--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Exercise5 {
-}
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+
+        int callNumber=0;
+        System.out.println("How many calls do you want to store?");
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        callNumber = inputValue.nextInt();
+        Call[] myCalls= new Call[callNumber];
+
+        //Getting the calls
+        for (int i = 0; (i < callNumber); i++) {
+            System.out.println("Introduce in order the source number, destination number, duration in seconds, the " +
+                    "band of the call, and L for local and P for provincial:");
+            int aux1=0, aux2=0, aux3=0, aux4=0;
+            char localLetter;
+            boolean isLocal=true;
+            inputValue = new Scanner(System.in);
+            aux1 = inputValue.nextInt();
+            inputValue = new Scanner(System.in);
+            aux2 = inputValue.nextInt();
+            inputValue = new Scanner(System.in);
+            aux3 = inputValue.nextInt();
+            inputValue = new Scanner(System.in);
+            aux4 = inputValue.nextInt();
+            inputValue = new Scanner(System.in);
+            localLetter = inputValue.next().charAt(0);
+            if (Character.toUpperCase(localLetter)=='L'){
+                isLocal=true;
+            } else if (Character.toUpperCase(localLetter)=='P') {
+                isLocal=false;
+            } else {
+                System.out.println("Error, you haven't introduced letter L o P");
+                break;
+            }
+
+            myCalls[i]= new Call(aux1,aux2,aux3,aux4, isLocal);
+            //System.out.println(myCalls[i]);
+            //System.out.println(myCalls[i].toString());
+            //System.out.println(myCalls[i].callCost());
+        }
+
+        //printing the calls
+        double turnOver=0;
+        for (int i = 0; (i < callNumber); i++) {
+            System.out.print(myCalls[i].toString());
+            System.out.println(" And that call costs: "+myCalls[i].callCost()/100+" €.");
+            turnOver += myCalls[i].callCost();
+        }
+        System.out.println("Total turnover is:"+turnOver/100+" €.");
+        }
+
+    }
